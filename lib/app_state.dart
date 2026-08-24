9abf4403650efa347a6bc182861ff1717eb89cd1
Nomi Developer <choudrymnouman@gmail.com>
2026-08-25 00:47:01 +0500

    Fix Flutter analyzer import warning

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
-import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
